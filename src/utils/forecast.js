e5d91deb9cd40763c6932e5c1201377d40ee6cbb
farook <farookahmed00@gmail.comm>
2023-01-16 07:29:32 +0600

    Remove unecessary console log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,8 +14,9 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
+       
       callback(
-        undefined,
+        undefined, 
         body.current.weather_descriptions[0] +
           ". it is currently " +
           body.current.temperature +
